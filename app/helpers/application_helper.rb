42172c9510725f0815165ca5dab286e194333dc4
Fcott <f.f.f.kt13@gmail.com>
2016-12-30 14:35:59 +0900

    flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def profile_picture(user)
     if user.image
       user.image
@@ -6,4 +7,29 @@ module ApplicationHelper
       'https://ukla.org/images/icons/user-icon.svg'
     end
   end
+
+ #  def alert_class_for flash_type
+ #   case flash_type
+ #     when :success
+ #       "alert-success"
+ #     when :error
+ #       "alert-error"
+ #     when :alert
+ #       "alert-block"
+ #     when :notice
+ #       "alert-info"
+ #     else
+ #       flash_type.to_s
+ #   end
+ # end
+
+ def alert_class_for(flash_type)
+  {
+    :success => 'alert-success',
+    :error => 'alert-danger',
+    :alert => 'alert-warning',
+    :notice => 'alert-info'
+  }[flash_type.to_sym] || flash_type.to_s
+end
+
 end
