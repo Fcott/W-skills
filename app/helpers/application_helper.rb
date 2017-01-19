88c609b1e06ad8d211f8e29c697a9a29d5a8122b
Fcott <f.f.f.kt13@gmail.com>
2017-01-19 17:09:25 +0900

    simple authentication using devise and  omniauth_facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  def profile_picture(user)
+  def user_picture(user)
     if user.image
       user.image
     else
@@ -8,20 +8,6 @@ module ApplicationHelper
     end
   end
 
- #  def alert_class_for flash_type
- #   case flash_type
- #     when :success
- #       "alert-success"
- #     when :error
- #       "alert-error"
- #     when :alert
- #       "alert-block"
- #     when :notice
- #       "alert-info"
- #     else
- #       flash_type.to_s
- #   end
- # end
 
  def alert_class_for(flash_type)
   {
