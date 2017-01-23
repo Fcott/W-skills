dfe1085996f97651e3256a4dee3b38f555392244
Fcott <f.f.f.kt13@gmail.com>
2017-01-24 00:02:30 +0900

    change @skills to @user_skills

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @skills = @user.user_skills.points_desc
-    @feature_skills = @skills.first(6)
+    @user_skills = @user.user_skills.points_desc
+    @feature_skills = @user_skills.first(6)
   end
 end
